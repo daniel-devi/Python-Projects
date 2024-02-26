2d65e8e11f3973677705637c37cf6bca5a949ebc
daniel-devi <devonaguh33@outlook.com>
2024-02-26 17:17:44 +0000

    updated maths

--- Maths Game with timer/Main.py
+++ Maths Game with timer/Main.py
@@ -6,6 +6,7 @@ MIN_OPERAND = 3
 MAX_OPERAND = 12
 TOTAL_PROBLEMS = 10
 
+highscore = 1000
 
 def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
@@ -17,22 +18,40 @@ def generate_problem():
     return expr, answer
 
 
-wrong = 0
-input("Press enter to start!")
-print("----------------------")
+start = input("Do you want to Play our Maths GAME : (Y)/(N)").upper()
 
-start_time = time.time()
+while start == "Y":
+    input("Press enter to start!")
+    wrong =0
+    print("----------------------")
 
-for i in range(TOTAL_PROBLEMS):
-    expr, answer = generate_problem()
-    while True:
-        guess = input("Problem #" + str(i + 1) + ": " + expr + " = ")
-        if guess == str(answer):
-            break
-        wrong += 1
+    start_time = time.time()
 
-end_time = time.time()
-total_time = round(end_time - start_time, 2)
+    for i in range(TOTAL_PROBLEMS):
+        expr, answer = generate_problem()
+        while True:
+            guess = input("Problem #" + str(i + 1) + ": " + expr + " = ")
+            if guess == str(answer):
+                break
+            wrong += 1
 
-print("----------------------")
-print("Nice work! You finished in", total_time, "seconds!")
+    end_time = time.time()
+    total_time = round(end_time - start_time, 2)
+
+    if total_time <= highscore:
+        print("################ New Highscore Achieved ########################")
+        highscore = total_time
+    else:
+        pass
+    if wrong == 0:
+        print("Yahh you got none Wrong")
+    else:
+        print(f"You got {wrong} Wrong")
+        
+
+    print(f"You Highscore is {highscore} seconds... ")
+    print("----------------------")
+    print("Nice work! You finished in", total_time, "seconds!")
+    print("----------------------")
+
+print("Thanks for Playing")
